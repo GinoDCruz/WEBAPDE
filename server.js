40c66e6282826bc4d78e396cde28b31d3930c14e
GinoDCruz <38290377+GinoDCruz@users.noreply.github.com>
2019-08-22 15:54:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(session({
 }))
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb+srv://fan34:fun34@type-racer-mli3n.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://fan34:can34@type-racer-mli3n.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser:true,
     useFindAndModify: false
 })
